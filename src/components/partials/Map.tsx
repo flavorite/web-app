4037d2f10014fd52b0848bcfdfc758828e4a169e
valerieyang00 <valerieyang00@gmail.com>
2022-11-23 13:30:34 -0800

    simple map added

--- src/components/partials/Map.tsx
+++ src/components/partials/Map.tsx
@@ -1,8 +1,37 @@
+import React, { useState } from 'react';
+import GoogleMapReact from 'google-map-react';
+import Marker from './Marker'
 
-export default function Map () {
+
+export default function Map(props: any) {
+    const API_KEY = process.env.REACT_APP_API_KEY
+    const getMapOptions = (maps: any) => {
+        return {
+          disableDefaultUI: true,
+          mapTypeControl: true,
+          streetViewControl: true,
+          styles: [{ featureType: 'poi', elementType: 'labels', stylers: [{ visibility: 'on' }] }],
+        };
+      };
+    const [center, setCenter] = useState({lat: 11.0168, lng: 76.9558 });
+    const [zoom, setZoom] = useState(11);
     return (
-        <div>
-            Google Map to insert to Home Page
-        </div>
-    )
+        <div style={{ height: '70vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{key: `${API_KEY}`}}
+          defaultCenter={center}
+          defaultZoom={zoom}
+          options={getMapOptions}
+        >
+          <Marker
+            lat={11.0168}
+            lng={76.9558}
+            name="My Marker"
+            color="blue"
+          />
+        </GoogleMapReact>
+      </div>
+    );
 }
+
+
