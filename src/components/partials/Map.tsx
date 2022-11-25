a7abe020ec5ab6d97a107e873e33134f85e9ac43
valerieyang00 <valerieyang00@gmail.com>
2022-11-25 11:23:19 -0800

    simple map implemented. studying the rest

--- src/components/partials/Map.tsx
+++ src/components/partials/Map.tsx
@@ -1,3 +1,28 @@
+import { GoogleMap, LoadScript, useJsApiLoader } from '@react-google-maps/api';
+
 export default function Map() {
-  return <div>Google Map to insert to Home Page</div>
+  const API_KEY = process.env.REACT_APP_API_KEY
+  const containerStyle = {
+    width: '100%',
+    height: '60vh'
+  };
+  
+  const center = {
+    lat: -3.745,
+    lng: -38.523
+  };
+  return (
+    <LoadScript
+    googleMapsApiKey={`${API_KEY}`}
+    >
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+    >
+      { /* Child components, such as markers, info windows, etc. */ }
+      <></>
+    </GoogleMap>
+  </LoadScript>
+  )
 }
