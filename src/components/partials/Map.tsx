49cc45e27aa9cdc690dbc7513a6a0c2f4ae27772
valerieyang00 <valerieyang00@gmail.com>
2022-11-26 15:54:13 -0800

    working on getting markers on restaurants using Nearbysearch

--- src/components/partials/Map.tsx
+++ src/components/partials/Map.tsx
@@ -1,18 +1,23 @@
-import { useState } from 'react'
-import { GoogleMap, LoadScript, Autocomplete, InfoWindow } from '@react-google-maps/api'
-const lib = 'places'
+import { useState, useEffect } from 'react'
+import { GoogleMap, useGoogleMap, LoadScript, LoadScriptProps, Autocomplete, InfoWindow } from '@react-google-maps/api'
+import { setMaxIdleHTTPParsers } from 'http'
+
+// declared library for 'places' here to avoid react warning for LoadScript performance
+const lib: LoadScriptProps['libraries'] = ['places']
 
 export default function Map() {
   const API_KEY = process.env.REACT_APP_API_KEY
+
   const containerStyle = {
     width: '100%',
     height: '60vh',
   }
 
-  const center = {
+  const radius = 1000;
+  const [center, setCenter] = useState({
     lat: -3.745,
     lng: -38.523,
-  }
+  })
 
   const position = { lat: 33.772, lng: -117.214 }
 
@@ -22,12 +27,12 @@ export default function Map() {
     padding: 15,
   }
 
-
   const onLoadInfo = (infoWindow: any) => {
     console.log('infoWindow: ', infoWindow)
   }
 
   const [search, setSearch] = useState<any>('')
+  const [map, setMap] = useState<any>(null)
 
   function onLoadSearch(autocomplete: any): void {
     setSearch(autocomplete)
@@ -35,26 +40,38 @@ export default function Map() {
 
   function onPlaceChanged() {
     if (search !== null) {
-
       const place = search.getPlace()
-
       const name = place.name
-
       const status = place.business_status
-
       const formattedAddress = place.formatted_address
+      const lat = place.geometry.location.lat()
+      const lng = place.geometry.location.lng()
+
+      setCenter({
+        lat: lat,
+        lng: lng,
+      })
 
       console.log(`Name: ${name}`)
       console.log(`Business Status: ${status}`)
       console.log(`Formatted Address: ${formattedAddress}`)
+      console.log(`lat: ${lat}, lng: ${lng}`)
     } else {
       alert('Please enter text')
     }
   }
 
+  useEffect(() => {
+    if(map) {
+      console.log(map)
+    }
+  },[map])
+
+
+
   return (
-    <LoadScript googleMapsApiKey={`${API_KEY}`} libraries={[lib]}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+    <LoadScript googleMapsApiKey={`${API_KEY}`} libraries={lib}>
+      <GoogleMap onLoad={(map) => setMap(map)} mapContainerStyle={containerStyle} center={center} zoom={15}>
         <InfoWindow onLoad={onLoadInfo} position={position}>
           <div style={divStyle}>
             <h1>InfoWindow</h1>
@@ -63,7 +80,7 @@ export default function Map() {
         <Autocomplete onLoad={onLoadSearch} onPlaceChanged={onPlaceChanged}>
           <input
             type='text'
-            placeholder='Customized your placeholder'
+            placeholder='Search for location...'
             style={{
               boxSizing: 'border-box',
               border: '1px solid transparent',
