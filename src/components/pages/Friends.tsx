58887c92f47614ab1ea22afb98a5d274392d203f
valerieyang00 <valerieyang00@gmail.com>
2022-12-23 15:27:41 -0800

    progress on Friends

--- src/components/pages/Friends.tsx
+++ src/components/pages/Friends.tsx
@@ -1,23 +1,83 @@
+import SyncIcon from '@mui/icons-material/Sync'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import FormGroup from '@mui/material/FormGroup'
 import Stack from '@mui/material/Stack'
+import Switch from '@mui/material/Switch'
+import Typography from '@mui/material/Typography'
+import { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import useFriends from '../../hooks/useFriends'
+import Spinner from '../partials/Spinner'
+import { UserContext } from '../partials/UserContext'
 
 export default function Friends() {
-  // TODO: replace with 'Friends' objects from useUser()
-  const friends = ['Jake', 'Jason', 'Jamie']
+  const { currentUser } = useContext(UserContext)
+  const username = currentUser!.username
+  const [enabled, setEnabled] = useState<boolean>(false)
+  const {
+    friends,
+    loading: loadingFriends,
+    error: errorFetchingFriends,
+  } = useFriends({ username: username })
+
+  useEffect(() => {
+    if (friends.length !== 0) {
+      setEnabled(true)
+    } else {
+      setEnabled(false)
+    }
+  }, [])
 
   const friendsList = friends.map((friend, id) => {
     return (
-      <Link to={`/${friend}`} key={id}>
-        {friend}
+      <Link to={`/${friend.username}`} key={id}>
+        {friend.username}
       </Link>
     )
   })
 
+  const handleToggle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEnabled(event.target.checked)
+    // TODO useUpdateFriends to enable/disable FB friends
+  }
+
+  const handleSync = () => {
+    // TODO useUpdateFriends to sync FB friends to latest state
+    console.log('sync')
+  }
+
   return (
-    <Container fixed>
-      {/* TODO: Enable user to connect to FB friends if not done yet */}
-      <Stack spacing={2}>{friendsList}</Stack>
-    </Container>
+    <Spinner loading={loadingFriends}>
+      <Container fixed>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={enabled}
+                onChange={handleToggle}
+                inputProps={{ 'aria-label': 'controlled' }}
+              />
+            }
+            label={enabled ? 'FB friends enabled' : 'Enable FB Friends'}
+          />
+        </FormGroup>
+        {enabled ? (
+          <Button variant='contained' endIcon={<SyncIcon />} onClick={handleSync}>
+            Sync
+          </Button>
+        ) : (
+          ''
+        )}
+        <Box>
+          <Typography role='error-message'>
+            {errorFetchingFriends ? `${errorFetchingFriends}` : ''}
+          </Typography>
+          <Stack spacing={2}>{friendsList}</Stack>
+        </Box>
+      </Container>
+    </Spinner>
   )
 }
