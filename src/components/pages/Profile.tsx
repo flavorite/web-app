6f1b883a76b9b53ada20574c5b095b422edab371
valerieyang00 <valerieyang00@gmail.com>
2022-12-27 15:09:50 -0800

    progress commit

--- src/components/pages/Profile.tsx
+++ src/components/pages/Profile.tsx
@@ -1,28 +1,36 @@
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import useUser from '../../hooks/useUser'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
+import { useContext, useState } from 'react'
+import Spinner from '../partials/Spinner'
+import { UserContext } from '../partials/UserContext'
 
 export default function Profile() {
-  // TODO: Need to obtain username from user login
-  const username = 'kitty'
-  const { user, loading, error } = useUser({ username })
+  const { currentUser } = useContext(UserContext)
+  const { username } = useParams()
+  const profileUsername = username!
+  const { user, loading: loadingUser, error: errorUser } = useUser({ username: profileUsername })
+
   return (
-    <Container>
-      <Typography variant='h3' component='h3'>
-        @{user.username}
-      </Typography>
-      <Button variant='contained'>
-        <Link to={`/${username}/friends`}>View Friends</Link>
-      </Button>
-      <Button variant='contained'>
-        <Link to={`/${username}/favorites`}>Favorite Dishes</Link>
-      </Button>
-      <Button variant='contained'>
-        <Link to={`/${username}/reviews`}>View all Reviews</Link>
-      </Button>
-      {/* TODO: Need to add Edit Profile , and User's recent reviews below Profile options */}
-    </Container>
+    <Spinner loading={loadingUser}>
+      <Container>
+        <Typography role='error-message-userData'>{errorUser ? `${errorUser}` : ''}</Typography>
+        <Typography variant='h3' component='h3'>
+          @{user.username}
+        </Typography>
+        <Button variant='contained'>
+          <Link to={`/${user.username}/friends`}>View Friends</Link>
+        </Button>
+        <Button variant='contained'>
+          <Link to={`/${user.username}/favorites`}>Favorite Dishes</Link>
+        </Button>
+        <Button variant='contained'>
+          <Link to={`/${user.username}/reviews`}>View all Reviews</Link>
+        </Button>
+        {/* TODO: Need to add Edit Profile , and User's recent reviews below Profile options */}
+      </Container>
+    </Spinner>
   )
 }
